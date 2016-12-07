43a9c7d154360889be3fe1da12aec6376d933e7e
bardiaborhani <bardia.borhani@gmail.com>
2016-12-07 13:13:27 -0800

    Update BST.cpp

--- BST.cpp
+++ BST.cpp
@@ -1,56 +1,134 @@
 #include "BST.h"
 #include <sstream>
 
+// This destructor calls the clear function which deletes all the nodes in the graph
+// The destructor is called when the delete operator is applied to a pointer to the object (delete *p where p is a pointer to this tree)
+// OR when the object goes out of scope
+//Pre-condition: Constructor has to already have been called (object has to have been made)
+//Post-condition: deletes the tree (deletes all the nodes in the tree) -- deletes all the clients
 BST::~BST(){
     clear(root);
 }
 
+// The clear function deletes all the nodes in the tree
+// It calls a facade (that passes in the root of the tree) so it can use recursion to traverse the tree and delete all the nodes
+// Pre-condition: the object has to already have been made
+// Post-condition: the tree is empty - there are no more nodes
 void BST::clear(){
 	return clear(root); // facade is created so recursion is able to happen (the function with a parameter can be used for recursion)
 }
 
+// This is the facade of the clear function - it passes in a node as a parameter and it uses recursion to traverse the tree and delete all the nodes
+// The root is passed in as the parameter when the function is first called - if the root isn't empty then the left and right side of the tree are deleted as well as the root
+// The pointer to the data and the node are what is deleted -- all the clients are deleted
+// If the root is null (has no data as well as no left and right nodes) then nothing happens - the if loop is skipped and the end of the function is reached
+// Pre-condition: The root of the tree is passed in as a parameter - it can have a value or it can be null - either way it is okay
+// Post-condition: All the pointers to data and nodes are deleted -- all the clients are deleted from the tree
 void BST::clear(BST::Node* subtree){
-    if(subtree!= nullptr){
-        clear(subtree->right);
-        clear(subtree->left);
-	delete subtree->data;
-        delete subtree;
+    if(subtree!= nullptr){	// if the subtree isn't empty then go through the tree and delete what is there
+        clear(subtree->right);	// traverse the right side of the tree
+        clear(subtree->left);	// traverse the left side of the tree
+	delete subtree->data;	// for every node, delete the data
+        delete subtree;		// delete all the nodes
     }
 }
 
+// This finds the height of the tree and returns a number representing the height of the tree
+// This function uses a facade to be able to use recursion to traverse through the list
+// Pre-condition: The object has to be already made
+// Post-condition: The facade is called and a int is returned representing the height of the tree
 int BST::height(){
 	return height(root);
 }
 
-// The height of a tree is the length of the path from the root to the deepest node in the tree. 
-// A (rooted) tree with only a node (the root) has a height of zero
-//The height of a binary search tree is equal to number of layers - 1.
+// This facade takes in a node - starting with the root- and traverses the tree using recursion
+// The tree steps through the nodes of the tree down until it reaches the leaf node
+// As it goes down, it counts the number of times it goes down - keeping track of the height of the tree
+// Pre-condition: A pointer to a node is passed into the parameter - doesn't have to have any value - can be set to null
+// Post-condition: An int is returned representing the height of the tree
 int BST::height(BST::Node* subtree){
+    // The height of a tree is the length of the path from the root to the deepest node in the tree. 
+    // A (rooted) tree with only a node (the root) has a height of zero
+    // The height of a binary search tree is equal to number of layers - 1
+
+    // the base case returns a 0 and is called when the node being looked at is null (has no data and left and right nodes)
+    // this happens when the traversal reaches the bottom of the tree (tries to go further down from a leaf)
+    // the else statement traverses through the tree an adds 1 to what is returned indicating how far (height) the node is down the tree
+    // the max function indicates which height of which side of the parent node is longer - the longest one is returned
     return subtree == nullptr ? 0 : 1 + max(height(subtree->left), height(subtree->right));
+
+    // Alternative way of writing the code above	
     /* if(subtree == nullptr){
     	return 0;
     } else {
     	return 1 + max(height(subtree->left), height(subtree->right));
     }
-    */
+    */ 
 }
 
+// This function calls a facade that returns an int indicating the number of nodes in the tree
+// The facade function passes in the root of the tree so it can start there before traversing through the tree
+// Pre-condition: A object has to be created for this class
+// Post-condition: An int is returned indicating the number of nodes in the tree
 int BST::size(){ 
 	return size(root);
 }
 
+// This facade for the size function passes in a node pointer (starting with the root node) and traverses the tree
+// A base case is created to stop the traversal once a subtree represents a pointer to a node that is set to null (has no data and child nodes)
+// As the tree is traversed - int 1 is returned as every node is reached - this indicates how many nodes are in the tree
+// After the tree is tranversed, an int is returned indicating the number of nodes in the tree
+// In this case, it indicates the number of clients in our bankman manager
+// Pre-condition: Object for class has to be made - root of the tree is passed through as the parameter
+// Post-condition: An int is retuned indicating the number of nodes (clients) in the tree
 int BST::size(BST::Node* subtree){
+    // the base case returns a 0 and is called when the node being looked at is null (has no data and left and right nodes)
+    // this happens when the traversal reaches the past a leaf node
+    // the else statement traverses through the tree an adds 1 every time it touches a new node
+    // the number of nodes on the left side and right side of the tree are added together with the root to find the number of nodes (clients) in the tree
     return subtree == nullptr ? 0 : 1 + size(subtree->left) + size(subtree->right);
+
+    // Alternative way of writing the code above	
+    /* if(subtree == nullptr){
+    	return 0;
+    } else {
+    	return 1 + size(subtree->left) + size(subtree->right);
+    }
+    */ 
 }
 
+
+// This insert function inserts a new node (new client) into the tree 
+// This function calls a facade that passes in the client (information for the new node) and the root of the tree
+// These are passed through so the facade can traverse recursively through the tree
+// Pre-condition: A pointer to a client object must be passed through this function
+// Post-condition: The pointer to the client object is inserted into the tree and the root of the tree is set to what the facade returns
+// indicating if there is a new root or if the root is the same
 void BST::insert(Client* client){
-    root = insert(client, root);
+    root = insert(client, root);	// the facade returns a pointer to a node representing the root of the tree
 };
 
+// The insert function facade passes in a pointer to a clident object and the root of the tree
+// The function uses recursion to traverse through the tree and insert the client pointer into the tree
+// Once the leaf of the binary search tree is reached, a new node is created as a child to that leaf node and it contains the data of the client pointer
+// Prec-condition: A pointer to a client object and a pointer to the root of the tree is passed into the function when it is first called
+// Post-condition: The pointer to the client object is inserted into the tree and the root of the tree is returned
 BST::Node* BST::insert(Client* client, BST::Node* subtree) {
-    if(subtree == nullptr) subtree = new Node(client, nullptr, nullptr);
-    else if(*client < *subtree->data) subtree->left = insert(client, subtree->left);
-    else subtree->right = insert(client, subtree->right);
+    if(subtree == nullptr){ // base case - a new node object is allocated and set as the subtree
+	    subtree = new Node(client, nullptr, nullptr);
+    }
+    else if(*client < *subtree->data){ 
+	    // if the client's account number is less than the account number of the client node being looked at, 
+	    // then traverse through the left side of the parent node until you reach a leaf node to then reach the base case
+	    // the left child of the subtree is assigned to what the recursive call returns - this keeps the tree intact
+	    // and when the node is inserted - this makes sure that the parent node is contected to this child node (the new node that is inserted) 
+	    subtree->left = insert(client, subtree->left); 
+    }
+    else{
+	    // if the client's account number is more than the account number of the client node being looked at, 
+	    // then traverse through the right side of the parent node until you reach a leaf node to then reach the base case
+	    subtree->right = insert(client, subtree->right);
+    }
     return subtree;
 }
 
