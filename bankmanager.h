f327860b99e22f161a2e1cd77a682a635b6c6a8c
Bardia Borhani <bardia.borhani@gmail.com>
2016-12-09 10:24:38 -0800

    asdasd

--- bankmanager.h
+++ bankmanager.h
@@ -32,7 +32,7 @@ private:
 public:
 
 	void readClients(ifstream& inFile); //passes in an ifstream that reads the client data file to and creates a BST of clients
-	void readTransactions(ifstream& inFile);
+	void readTransactions(ifstream& inFile); //
 	void transact(void);
 	void displayClients(void);
 	void displayTransactions(void);
