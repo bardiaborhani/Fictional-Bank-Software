cbd482a43d2379e64fe3bffa1ffe40e9254644cb
moromis <moromise@gmail.com>
2016-12-10 18:25:56 -0800

    commented main, added assumptions

--- main.cpp
+++ main.cpp
@@ -4,13 +4,47 @@ Program created by:
 Kevin Ulrich
 Bardia Borhani
 
-PURPOSE: Write software to handle transactinos for a local financial firm called Lots of Savings Earned (LOSE)
-LOST wishes to be able to display the contents of the firm at any time and allow clients to perfom transactinos on their various accounts
-ASSUMPTION: It is assumed that the format (number and types of input items) is correct for the data file containing the commands
+December 11th, 2016
 
-Main for Homework 5. Creates new BankManager object and passes two file streams to this object.
-Then displays the clients built by the BankManager as well as the transactions that have been
-completed.
+PURPOSE: 
+
+	Write software to handle transactions for a local financial firm called Lots of Savings Earned (LOSE)
+	LOSE wishes to be able to display the contents of the firm at any time and allow clients to perfom transactions 
+	on their various accounts.
+
+ASSUMPTIONS: 
+
+	It is assumed that all data text files are formatted correctly. All text files, if this program is being run
+	on the Windows OS, must contain a line after the data for EOF purposes.
+
+	For data files, the clients should be formatted as follows: 
+
+		* lastName firstName account1 account2 account3 account4 account5 account6 account7 account8 account9 account10
+
+		where all 10 accounts are accounted for, are integers and no chars occur after the client's first name and last name.
+		The accounts should be in correct order as specified in the design document (i.e. account1 is Money Market, account2
+		is Prime Money Market, etc). All data for a single client should be one one line of the text file.
+
+	For command files, the transactions should be formatted in one of the four ways:
+	NOTE that clientID and accountID are concatenated, such as 12341, where 1234 is the client ID, and 1 is the account ID.
+
+		* D clientIDaccountID amount
+	
+		* W clientIDaccountID amount
+
+		* M clientIDaccountID amount clientIDaccountID
+
+		* H clientIDaccountID
+
+
+	any ints used in the data files must not exceed 2147483647, which is the max of an integer. over this amount, erroneous behaviour
+	will occur.
+
+DESCRIPTION:
+
+	Main for Homework 5. Creates new BankManager object and passes two file streams to this object.
+	Then displays the clients built by the BankManager as well as the transactions that have been
+	completed.
 
 ------------------------------------------------------------------------------------------------------*/
 
@@ -19,23 +53,26 @@ completed.
 #include <fstream>
 
 #include "bankManager.h"
-#include "client.h"
-#include "transaction.h"
-#include "bankqueue.h"
-
 
 using namespace std;
 
 int main() {
 
-	BankManager bankManager;						//instantiates BankManager object
-	ifstream clientFile;							//creates new ifstream
-	clientFile.open("testdata2.txt");					//opens the data file
+	//instantiates BankManager object, which oversees all high level operations
+	//regarding Clients and the transactions that occur between/to them.
+	BankManager bankManager;
 
- 	bankManager.readClients(clientFile);			//passes the client data file to bankManger
-													//to construct the BST
+	//create an ifstream and open the data text file to read into
+	//the BankManager object.
+	ifstream clientFile;
+	clientFile.open("testdata2.txt");
 
-	//create and open an ifstream in order
+	//passes the client data file to bankManger
+	//to construct the BST
+	bankManager.readClients(clientFile);
+
+	//create an ifstream and open the commands text file to read into
+	//the BankManager object.
 	ifstream transactionFile;
 	transactionFile.open("testcommand2.txt");
 
@@ -60,5 +97,6 @@ int main() {
 	//closes the ifstreams
 	clientFile.close();
 	transactionFile.close();
+
 	return 0;
 };
